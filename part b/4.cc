53646c89984bda388cab4f0b6efab5e415f54b11
ananyaananth29 <67549566+ananyaananth29@users.noreply.github.com>
2022-07-07 23:11:44 +0530

    Update 4.cc

--- part b/4.cc
+++ part b/4.cc
@@ -1,74 +1,59 @@
-#include"ns3/netanim-module.h"
-#include"ns3/core-module.h"
-#include"ns3/network-module.h"
-#include"ns3/internet-module.h"
-#include"ns3/point-to-point-module.h"
-#include "ns3/csma-module.h"
-#include"ns3/applications-module.h"
-#include"ns3/wifi-module.h"
-#include "ns3/mobility-module.h"
+
+#include <string>
+#include <fstream>
+#include "ns3/core-module.h"
+#include "ns3/point-to-point-module.h"
+#include "ns3/internet-module.h"
+#include "ns3/applications-module.h"
+#include "ns3/network-module.h"
+#include "ns3/packet-sink.h"
+#include "ns3/netanim-module.h"
 
 using namespace ns3;
+int
+main (int argc, char *argv[])
+{
 
-int main(int argc,char *argv[]){
-  NodeContainer ptpnodes;
-  ptpnodes.Create (2);
+
+
+ NodeContainer nodes;
+ nodes.Create (2);
+ 
+ PointToPointHelper pointToPoint;
+ pointToPoint.SetDeviceAttribute ("DataRate", StringValue ("500Kbps"));
+ pointToPoint.SetChannelAttribute ("Delay", StringValue ("5ms"));
+ 
+ NetDeviceContainer devices;
+ devices = pointToPoint.Install (nodes);
+ 
+ InternetStackHelper internet;
+ internet.Install (nodes);
+ 
+ Ipv4AddressHelper ipv4;
+ ipv4.SetBase ("10.1.1.0", "255.255.255.0");
+ 
+ Ipv4InterfaceContainer i = ipv4.Assign (devices);
+ 
+
+ BulkSendHelper source ("ns3::TcpSocketFactory", InetSocketAddress (i.GetAddress (1), 9));
+ 
+ source.SetAttribute ("MaxBytes", UintegerValue (0));
+ 
+ ApplicationContainer sourceApps = source.Install (nodes.Get (0));
+ sourceApps.Start (Seconds (0.0));
+ sourceApps.Stop (Seconds (10.0));
+ 
+ PacketSinkHelper sink ("ns3::TcpSocketFactory", InetSocketAddress (Ipv4Address::GetAny (), 9));
+ 
+ ApplicationContainer sinkApps = sink.Install (nodes.Get (1));
+ sinkApps.Start (Seconds (0.0));
+ sinkApps.Stop (Seconds (10.0));
+ 
+ Simulator::Stop (Seconds (10.0));
+ AnimationInterface anim ("fourth.xml"); 
+ anim.EnablePacketMetadata(true);
+ Simulator::Run ();
+ Simulator::Destroy ();
   
-  NodeContainer csmanodes;
-  csmanodes.Add (ptpnodes.Get (1));
-  csmanodes.Create (3);
-  
-  PointToPointHelper ptp;
-  ptp.SetDeviceAttribute ("DataRate",StringValue ("5Mbps"));
-  ptp.SetChannelAttribute ("Delay",StringValue ("2ms"));
-  
-  CsmaHelper csma;
-  csma.SetChannelAttribute ("DataRate",StringValue ("100Mbps"));
-  csma.SetChannelAttribute ("Delay",StringValue ("6560ns"));
-  
-  NetDeviceContainer ptpdevices;
-  ptpdevices=ptp.Install (ptpnodes);
-  
-  NetDeviceContainer csmadevices;
-  csmadevices=csma.Install (csmanodes);
-  
-  InternetStackHelper stack;
-  stack.Install (ptpnodes.Get (0));
-  stack.Install (csmanodes);
-  
-  Ipv4AddressHelper ptpaddress;
-  ptpaddress.SetBase ("10.1.1.0","255.255.255.0");
-  
-  Ipv4AddressHelper csmaaddress;
-  csmaaddress.SetBase ("10.1.1.0","255.255.255.0");
-  
-  Ipv4InterfaceContainer ptpinterfaces;
-  ptpinterfaces=ptpaddress.Assign (ptpdevices);
-  
-  Ipv4InterfaceContainer csmainterfaces;
-  csmainterfaces=csmaaddress.Assign (csmadevices);
-  
-  UdpEchoServerHelper echoServer (9);
-  
-  ApplicationContainer serverApps =echoServer.Install (csmanodes.Get (3));
-  serverApps.Start ( Seconds (1.0));
-  serverApps.Stop ( Seconds (10.0));
-  
-  UdpEchoClientHelper echoClient (csmainterfaces.GetAddress (3), 9);
-  echoClient.SetAttribute ("MaxPackets",UintegerValue (1));
-  echoClient.SetAttribute ("Interval",TimeValue ( Seconds (1.0)));
-  echoClient.SetAttribute ("PacketSize",UintegerValue (1024));
-  
-  ApplicationContainer clientApps =echoServer.Install (ptpnodes.Get (0));
-  clientApps.Start ( Seconds (2.0));
-  clientApps.Stop ( Seconds (10.0));
-  
-  Ipv4GlobalRoutingHelper::PopulateRoutingTables ();
-  ptp.EnablePcapAll ("second");
-  csma.EnablePcap ("second",csmadevices.Get (1), true);
-  
-  AnimationInterface anim ("anithird.xml");
-  Simulator::Run ();
-  Simulator::Destroy ();
-  return 0;
-}
+
+  }
