a0e28a340940f4690ca4f62661a7b92c07bd2942
ananyaananth29 <67549566+ananyaananth29@users.noreply.github.com>
2022-07-07 23:35:38 +0530

    Update 4.cc

--- part b/4.cc
+++ part b/4.cc
@@ -1,4 +1,3 @@
-
 #include <string>
 #include <fstream>
 #include "ns3/core-module.h"
@@ -15,45 +14,36 @@ main (int argc, char *argv[])
 {
 
 
-
- NodeContainer nodes;
+ uint32_t maxBytes = 0;
+NodeContainer nodes;
  nodes.Create (2);
- 
- PointToPointHelper pointToPoint;
- pointToPoint.SetDeviceAttribute ("DataRate", StringValue ("500Kbps"));
- pointToPoint.SetChannelAttribute ("Delay", StringValue ("5ms"));
- 
- NetDeviceContainer devices;
- devices = pointToPoint.Install (nodes);
- 
- InternetStackHelper internet;
+PointToPointHelper pointToPoint;
+  pointToPoint.SetDeviceAttribute ("DataRate", StringValue ("500Kbps"));
+  pointToPoint.SetChannelAttribute ("Delay", StringValue ("5ms"));
+NetDeviceContainer devices;
+  devices = pointToPoint.Install (nodes);
+InternetStackHelper internet;
  internet.Install (nodes);
- 
- Ipv4AddressHelper ipv4;
+Ipv4AddressHelper ipv4;
  ipv4.SetBase ("10.1.1.0", "255.255.255.0");
- 
  Ipv4InterfaceContainer i = ipv4.Assign (devices);
- 
-
- BulkSendHelper source ("ns3::TcpSocketFactory", InetSocketAddress (i.GetAddress (1), 9));
- 
- source.SetAttribute ("MaxBytes", UintegerValue (0));
- 
- ApplicationContainer sourceApps = source.Install (nodes.Get (0));
- sourceApps.Start (Seconds (0.0));
- sourceApps.Stop (Seconds (10.0));
- 
- PacketSinkHelper sink ("ns3::TcpSocketFactory", InetSocketAddress (Ipv4Address::GetAny (), 9));
- 
- ApplicationContainer sinkApps = sink.Install (nodes.Get (1));
- sinkApps.Start (Seconds (0.0));
- sinkApps.Stop (Seconds (10.0));
- 
- Simulator::Stop (Seconds (10.0));
- AnimationInterface anim ("fourth.xml"); 
- anim.EnablePacketMetadata(true);
+uint16_t port = 9;  // well-known echo port number
+ BulkSendHelper source ("ns3::TcpSocketFactory",
+ InetSocketAddress (i.GetAddress (1), port));
+  source.SetAttribute ("MaxBytes", UintegerValue (maxBytes));
+  ApplicationContainer sourceApps = source.Install (nodes.Get (0));
+  sourceApps.Start (Seconds (0.0));
+  sourceApps.Stop (Seconds (10.0));
+PacketSinkHelper sink ("ns3::TcpSocketFactory",
+  InetSocketAddress (Ipv4Address::GetAny (), port));
+  ApplicationContainer sinkApps = sink.Install (nodes.Get (1));
+  sinkApps.Start (Seconds (0.0));
+  sinkApps.Stop (Seconds (10.0));
+Simulator::Stop (Seconds (10.0));
+AnimationInterface anim ("fourth.xml"); 
+anim.EnablePacketMetadata(true);
  Simulator::Run ();
- Simulator::Destroy ();
+  Simulator::Destroy ();
   
 
   }
