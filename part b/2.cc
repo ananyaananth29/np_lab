2315982426fb501762c99a76c5122735b34a9e1f
ananyaananth29 <67549566+ananyaananth29@users.noreply.github.com>
2022-07-07 23:33:42 +0530

    Update 2.cc

--- part b/2.cc
+++ part b/2.cc
@@ -7,48 +7,50 @@
 
 using namespace ns3;
 
-int main (int argc, char *argv[])
+int 
+main (int argc, char *argv[])
 {
-  NodeContainer nodes;
-  nodes.Create (4);
-  
-  CsmaHelper csma;
-  csma.SetChannelAttribute ("DataRate", StringValue ("5Mbps"));
-  csma.SetChannelAttribute ("Delay", StringValue ("2ms"));
+  Address serverAddress;
+NodeContainer n;
+  n.Create (4);
+InternetStackHelper internet;
+  internet.Install (n);
+CsmaHelper csma;
+  csma.SetChannelAttribute ("DataRate", DataRateValue (DataRate (5000000)));
+  csma.SetChannelAttribute ("Delay", TimeValue (MilliSeconds (2)));
   csma.SetDeviceAttribute ("Mtu", UintegerValue (1400));
-  
-  NetDeviceContainer devices 
-  devices = csma.Install (nodes);
-  
-  InternetStackHelper stack;
-  stack.Install (nodes);
-  
-  Ipv4AddressHelper address;
-  address.SetBase ("10.1.1.0", "255.255.255.0");
-  
-  Ipv4InterfaceContainer interfaces 
-  interfaces = address.Assign (devices);
+  NetDeviceContainer d = csma.Install (n);
+Ipv4AddressHelper ipv4;
+      ipv4.SetBase ("10.1.1.0", "255.255.255.0");
+      Ipv4InterfaceContainer i = ipv4.Assign (d);
+      serverAddress = Address(i.GetAddress (1));
    
-  UdpEchoServerHelper echoServer (9);
-  
-  ApplicationContainer serverApps = echoServer.Install (nodes.Get (1));
-  serverApps.Start (Seconds (1.0));
-  serverApps.Stop (Seconds (10.0));
-
-  
-  UdpEchoClientHelper echoClient (interfaces.GetAddress (1), 9);
-  echoClient.SetAttribute ("MaxPackets", UintegerValue (1));
-  echoClient.SetAttribute ("Interval", TimeValue ( Seconds (1.0)));
-  echoClient.SetAttribute ("PacketSize", UintegerValue (1024));
-  
-  ApplicationContainer clientApps = echoClient.Install (nodes.Get (0));
-  clientApps.Start (Seconds (2.0));
-  clientApps.Stop (Seconds (10.0));
+  uint16_t port = 9;  // well-known echo port number
+  UdpEchoServerHelper server (port);
+  ApplicationContainer apps = server.Install (n.Get (1));
+  apps.Start (Seconds (1.0));
+  apps.Stop (Seconds (10.0));
 
 
-  AnimationInterface anim ("second.xml");
+  uint32_t packetSize = 1024;
+  uint32_t maxPacketCount = 1;
+  Time interPacketInterval = Seconds (1.);
+  UdpEchoClientHelper client (serverAddress, port);
+  client.SetAttribute ("MaxPackets", UintegerValue (maxPacketCount));
+  client.SetAttribute ("Interval", TimeValue (interPacketInterval));
+  client.SetAttribute ("PacketSize", UintegerValue (packetSize));
+  apps = client.Install (n.Get (0));
+  apps.Start (Seconds (2.0));
+  apps.Stop (Seconds (10.0));
+
+#if 0
+client.SetFill (apps.Get (0), "Hello World");
+client.SetFill (apps.Get (0), 0xa5, 1024);
+uint8_t fill[] = { 0, 1, 2, 3, 4, 5, 6};
+  client.SetFill (apps.Get (0), fill, sizeof(fill), 1024);
+#endif
+AnimationInterface anim ("second.xml");
   Simulator::Run ();
   Simulator::Destroy ();
-  return 0;
   
 }
