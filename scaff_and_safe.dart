8220e0a5fc1ee9d61e95dc7adc250efea8fa77cb
ananyaananth29 <67549566+ananyaananth29@users.noreply.github.com>
2023-01-13 01:50:26 +0530

    Create scaff_and_safe.dart

---
+++ scaff_and_safe.dart
@@ -0,0 +1,1 @@
+void main() { runApp(MaterialApp( home: Scaffold( // Scaffold houses appBar and body property appBar:AppBar( title: Text( 'Scaffold-SafeArea' ), ), body:Container( child: SafeArea( // Defining SafeArea as Child of Container child: Center( child: Text( 'This is a Sample Text' ), ), ), ), ), )); }
